527eded9a6f72acb57a3662872278189048010ed
EllieChoi1998 <ellie19981008@gmail.com>
2024-09-20 10:59:30 +0900

    backend cors 설정 완료, 프론트와 연결 잘됨

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.demo.config;
-import com.example.demo.oAuth.CustomOAuth2LoginSuccessHandler;
-import com.example.demo.oAuth.CustomOAuth2UserService;
-import com.nimbusds.jose.jwk.source.ImmutableSecret;
+//import com.example.demo.oAuth.CustomOAuth2LoginSuccessHandler;
+//import com.example.demo.oAuth.CustomOAuth2UserService;
+//import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.oauth2.jwt.JwtEncoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -26,16 +26,16 @@ import org.springframework.security.web.firewall.StrictHttpFirewall;
 @Log4j2
 public class SecurityConfig  {
     private final WebConfig webConfig;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint; // 커스텀 엔트리 포인트 주입
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint; // 커스텀 엔트리 포인트 주입
 
     @Value("${security.jwt.secret-key}")
     private String jwtSecretKey;
 
-    @Bean
-    public JwtEncoder jwtEncoder() {
-        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtSecretKey.getBytes()));
-    }
+//    @Bean
+//    public JwtEncoder jwtEncoder() {
+//        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtSecretKey.getBytes()));
+//    }
     @Bean
     public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
@@ -61,15 +61,12 @@ public class SecurityConfig  {
                 .addFilterAfter(new CsrfCookieGeneratorFilter(), org.springframework.security.web.csrf.CsrfFilter.class)
                 .authorizeHttpRequests((request) ->
                         request.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                                .requestMatchers("/", "/api/**","/ws/**", "/auth/**", "/js/**", "/css/**", "/images/**", "http://localhost:3000/**", "/**").permitAll()
+                                .requestMatchers("/", "/api/**","/ws/**", "/auth/**", "/js/**", "/css/**", "/images/**", "http://localhost:80/**", "/**").permitAll()
                                 .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2->oauth2
-//                        .loginPage("http://localhost:3000")        // 인증되지 않은 사용자를 리다이렉트 하도록
-                        .successHandler(new CustomOAuth2LoginSuccessHandler())
-//                        .defaultSuccessUrl("http://localhost:3000/login-success") // 인증 성공 후 리다이렉트할 Vue.js 페이지
-
-                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService)));
+                );
+//                .oauth2Login(oauth2->oauth2
+//                        .successHandler(new CustomOAuth2LoginSuccessHandler())
+//                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService)));
 
 
         return http.build();
--- src/main/java/com/example/demo/oAuth/UserSecurityService.java
+++ src/main/java/com/example/demo/oAuth/UserSecurityService.java
@@ -1,58 +1,58 @@
-package com.example.demo.oAuth;
-
-import com.example.demo.domains.member.Member;
-import com.example.demo.domains.member.MemberRepository;
-import com.example.demo.domains.member.MemberRole;
-import com.example.demo.domains.member.MemberService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RequiredArgsConstructor
-@Service
-public class UserSecurityService implements UserDetailsService {
-
-    private final MemberRepository memberRepository;
-    private final MemberService memberService;
-    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // PasswordEncoder 추가
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        System.out.println("Trying to load user by email: " + email);
-        Member siteUser = this.memberService.findByEmail(email);
-//        Member _siteUser = this.memberRepository.findByEmail(email);
-
-//        if (_siteUser.isempty()) {
-//            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+//package com.example.demo.oAuth;
+//
+//import com.example.demo.domains.member.Member;
+//import com.example.demo.domains.member.MemberRepository;
+//import com.example.demo.domains.member.MemberRole;
+//import com.example.demo.domains.member.MemberService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class UserSecurityService implements UserDetailsService {
+//
+//    private final MemberRepository memberRepository;
+//    private final MemberService memberService;
+//    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // PasswordEncoder 추가
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        System.out.println("Trying to load user by email: " + email);
+//        Member siteUser = this.memberService.findByEmail(email);
+////        Member _siteUser = this.memberRepository.findByEmail(email);
+//
+////        if (_siteUser.isempty()) {
+////            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+////        }
+////        Member siteUser = _siteUser.get();
+////        memberRepository.updateLoginTime(siteUser, LocalDateTime.now());
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        if (siteUser != null) {
+//            authorities.add(new SimpleGrantedAuthority(MemberRole.LOGIN.getKey()));
+//        } else {
+//            authorities.add(new SimpleGrantedAuthority(MemberRole.SIGNUP.getKey()));
 //        }
-//        Member siteUser = _siteUser.get();
-//        memberRepository.updateLoginTime(siteUser, LocalDateTime.now());
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (siteUser != null) {
-            authorities.add(new SimpleGrantedAuthority(MemberRole.LOGIN.getKey()));
-        } else {
-            authorities.add(new SimpleGrantedAuthority(MemberRole.SIGNUP.getKey()));
-        }
-
-
-        // Logging the authorities
-        System.out.println("User authorities: " + authorities);
-
-        return new User(siteUser.getEmail(), siteUser.getName(), authorities);
-    }
-
-    // 비밀번호 확인 메서드 추가
-    public boolean checkPassword(String rawPassword, String encodedPassword) {
-        return passwordEncoder.matches(rawPassword, encodedPassword);
-    }
-}
+//
+//
+//        // Logging the authorities
+//        System.out.println("User authorities: " + authorities);
+//
+//        return new User(siteUser.getEmail(), siteUser.getName(), authorities);
+//    }
+//
+//    // 비밀번호 확인 메서드 추가
+//    public boolean checkPassword(String rawPassword, String encodedPassword) {
+//        return passwordEncoder.matches(rawPassword, encodedPassword);
+//    }
+//}
