0e2d27cea63ebc42ac6f1ca38668ac6be73eb01d
SunjuNa <141652391+SunjuNa@users.noreply.github.com>
2024-09-25 19:11:19 +0900

    Product부분 다희씨거랑 조정

--- src/main/java/com/example/demo/domains/member/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/demo/domains/member/repository/ShoppingCartRepository.java
@@ -1,14 +1,9 @@
 package com.example.demo.domains.member.repository;
 
 import com.example.demo.domains.member.entity.Member;
-import com.example.demo.domains.member.entity.Product;
 import com.example.demo.domains.member.entity.ShoppingCart;
-import com.example.demo.domains.member.entity.ShoppingCartProduct;
 import com.example.demo.domains.member.repository.querydsl.customs.ShoppingCartRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * author : 나선주
