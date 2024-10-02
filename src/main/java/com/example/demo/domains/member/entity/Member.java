1b7fec7e89f730143af57f659f2bf4e2e1127ed6
SunjuNa <141652391+SunjuNa@users.noreply.github.com>
2024-10-02 18:03:58 +0900

    주석 추가

--- src/main/java/com/example/demo/domains/member/entity/Member.java
+++ src/main/java/com/example/demo/domains/member/entity/Member.java
@@ -34,9 +34,9 @@ public class Member   {//implements UserDetails
     private long member_id;
 
     @Column(name="member_name")
-    private String name;
+    private String name;//진짜이름
 
-    private String username;
+    private String username;//아이디
 
     @Column(name="member_email")
     private String email;
