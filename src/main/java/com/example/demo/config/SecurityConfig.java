b0d8350e99575d5aa5491e808420dcb3b1c7a4cd
EllieChoi1998 <ellie19981008@gmail.com>
2024-10-16 09:23:40 +0900

    테스트중

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig  {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/","/auth/**", "/join", "/auth/login", "/auth/register", "/auth/register/email", "/auth/register/fetch_ids").permitAll()
+                        .requestMatchers("/login", "/","/auth/**", "/join", "/auth/login", "/auth/register", "/auth/register/email", "/auth/register/fetch_ids", "/admin222").permitAll()
                         .anyRequest().authenticated());
 
         http
